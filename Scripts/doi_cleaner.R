a7ae2df0ad5097ea559a10a8cd6b46d025e5bceb
Arthur Ferreira <arthur_sf@icloud.com>
2023-11-21 14:52:51 -0300

    Commit.

--- Scripts/doi_cleaner.R
+++ Scripts/doi_cleaner.R
@@ -4,22 +4,34 @@ doi_cleaner <- function(dois) {
   if (!sjmisc::is_empty(dois)) {
     for (j in 1:dim(clean_doi)[1]) {
       # remove square brackets from start-end
-      if (length(grep("\\[|\\]", "", clean_doi$DOI[j])) == 0) {
+      if (length(grep("\\[|\\]", clean_doi$DOI[j])) == 0) {
         clean_doi$DOI[j] <- gsub("\\[|\\]", "", clean_doi$DOI[j])
       }
       # remove 'doi:' prefix
-      if (length(grep("doi:", "", clean_doi$DOI[j])) == 0) {
+      if (length(grep("doi:", clean_doi$DOI[j])) == 0) {
         clean_doi$DOI[j] <- gsub("doi:", "", clean_doi$DOI[j])
       }
       # remove 'http://dx.doi.org/' prefix
-      if (length(grep("http://dx.doi.org/", "", clean_doi$DOI[j])) == 0) {
+      if (length(grep("http://dx.doi.org/", clean_doi$DOI[j])) == 0) {
         clean_doi$DOI[j] <-
           gsub("http://dx.doi.org/", "", clean_doi$DOI[j])
       }
       # remove 'https://doi.org/' prefix
-      if (length(grep("https://doi.org/", "", clean_doi$DOI[j])) == 0) {
+      if (length(grep("https://doi.org/", clean_doi$DOI[j])) == 0) {
         clean_doi$DOI[j] <-
           gsub("https://doi.org/", "", clean_doi$DOI[j])
+      }
+      # remove websites
+      if (length(grep("http://", clean_doi$DOI[j])) != 0) {
+        clean_doi$DOI[j] <- NA
+      }
+      # remove websites
+      if (length(grep("https://", clean_doi$DOI[j])) != 0) {
+        clean_doi$DOI[j] <- NA
+      }
+      # remove elements starting with www.
+      if (length(grep("^www.", clean_doi$DOI[j])) != 0) {
+        clean_doi$DOI[j] <- NA
       }
     }
   }
